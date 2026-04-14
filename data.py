6c090b5501468e1d83bff9acf0ce99777d3f8c3c
Jayanthi1326 <jayanthi06gnanaraj@gmail.com>
2026-04-14 19:20:48 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+DBMS stands for Database Management System.
+It is used to store and manage data.
+
+Operating System manages hardware and software resources.
+
+Artificial Intelligence is the simulation of human intelligence.
+
+Python is widely used in AI and data science.
